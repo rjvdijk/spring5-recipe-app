548c83c5ec3a87d6b6f5c1314c3cdd059f72249d
Rob van Dijk <rob@optimust.nl>
2021-02-26 11:27:57 +0100

    Lesson 193. Display a Recipe by ID.

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -25,4 +26,13 @@ public class RecipeServiceImpl implements RecipeService {
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
+
+    public Recipe findById(Long l) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+        if (!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Not found!");
+        }
+        return recipeOptional.get();
+    }
+
 }
