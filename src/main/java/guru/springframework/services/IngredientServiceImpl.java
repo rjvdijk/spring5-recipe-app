68b13c2584588580245cd8863a028f6b01011a3e
Rob van Dijk <rob@optimust.nl>
2021-03-12 13:53:56 +0100

    Lesson 203. Show ingredient.

---
+++ src/main/java/guru/springframework/services/IngredientServiceImpl.java
@@ -0,0 +1,42 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.IngredientCommand;
+import guru.springframework.converters.IngredientToIngredientCommand;
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class IngredientServiceImpl implements IngredientService {
+
+    private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final RecipeRepository recipeRepository;
+
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+        this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
+        Optional<Recipe> recipeOpional = recipeRepository.findById(recipeId);
+        if (!recipeOpional.isPresent()) {
+            // todo impl errorhandling
+            log.error("Recipe id not found: " + recipeId);
+        }
+        Recipe recipe = recipeOpional.get();
+        Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients().stream()
+                .filter(ingredient -> ingredient.getId().equals(ingredientId))
+                .map(ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
+        if (!ingredientCommandOptional.isPresent()) {
+            // todo impl errorhandling
+            log.error("Ingredient id not found: " + ingredientId);
+        }
+        return ingredientCommandOptional.get();
+    }
+
+}
